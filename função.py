e8149fec6f3efeb84ba9798c030c7c895c87dd2d
Klaus Pereira <pereiraklaus85@gmail.com>
2025-10-10 20:39:59 -0300

    Delete função.py

--- função.py
+++
@@ -1,8 +0,0 @@
-def verificar_par_impar(numero):
-    """
-    Recebe um número inteiro e retorna se ele é 'Par' ou 'Ímpar'.
-    """
-    if numero % 2 == 0:
-        return "Par"
-    else:
-        return "Ímpar"
